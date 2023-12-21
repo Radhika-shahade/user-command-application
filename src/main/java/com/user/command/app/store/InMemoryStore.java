13ddd60b814d3d432181eedefdcd2fc242d82a89
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-21 20:26:08 +0530

    Command line argument project

---
+++ src/main/java/com/user/command/app/store/InMemoryStore.java
@@ -0,0 +1,10 @@
+package com.user.command.app.store;
+
+import com.user.command.app.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryStore {
+    public static List<User> users = new ArrayList<User>();
+}
